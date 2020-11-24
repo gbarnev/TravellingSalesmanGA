fc140c97a9ac9e1e97b2412972bb5029c24d6eb9
Georgi Barnev <gebarnev@progress.com>
2020-11-24 02:19:17 +0200

    Change population count constant to 100

--- TravellingSalesmanGA/Constants.cs
+++ TravellingSalesmanGA/Constants.cs
@@ -7,7 +7,7 @@ namespace TravellingSalesmanGA
     public static class Constants
     {
         public const int IterationsCount = 5000;
-        public const int PopulationCount = 10;
+        public const int PopulationCount = 100;
         public const double ChildrenCountPercentage = 0.75;
         public const double MutationChance = 0.5;
         public const double CrossoverTopPercent = 0.4;
